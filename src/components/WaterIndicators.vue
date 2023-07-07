2a6230bb6de6e9b886b767ee0f721f1cf2fa4d6f
AlexandrKomisar <monolitalex@gmail.com>
2023-07-07 23:07:35 +0300

    0.01

---
+++ src/components/WaterIndicators.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>water</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WaterIndicators',
+  props: {
+    msg: String
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WaterIndicators/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import WaterIndicators from '@/components/WaterIndicators.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    WaterIndicators
   }
 }
 </script>
